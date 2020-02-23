423afae345cfc4c6c7e439fbdc18d20bc6fa8820
Ian Nastajus <ian.nastajus@gmail.com>
2020-02-22 23:20:22 -0500

    clean up

--- creeate.sql
+++ creeate.sql
@@ -1,5 +1,6 @@
 -- SHOW VARIABLES LIKE 'secure_file_priv';
--- once run, please copy manually all CSV files to directory, eg may be:
+-- run once to discover valid upload folder
+-- once found, please copy manually all CSV files to directory, eg may be:
 -- C:\ProgramData\MySQL\MySQL Server 5.7\Uploads\
 
 
@@ -18,16 +19,10 @@ CREATE TABLE got (
     sentence VARCHAR(2048) NOT NULL
 );
 
--- LOAD DATA INFILE 'c:/tmp/discounts.csv'
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/scripts/game_of_thrones/game-of-thrones-script-all-seasons/Game_of_Thrones_Script.csv'
     INTO TABLE got
     FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
     LINES TERMINATED BY '\n'
     IGNORE 1 LINES -- ;
--- IGNORE 1 ROWS;
--- (@c1, c2, c3, c4, c5, c6);
--- (@c1);
--- (@release_date)
 (@release_date, season, episode, episode_title, name_full, sentence)
--- SET release_date = convert_excel_serial_number_to_date_time(@release_date);
 SET release_date = STR_TO_DATE(@release_date, '%Y-%m-%d');
