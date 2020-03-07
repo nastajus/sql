f667bd2de13f72b3bba62396a191ccc0cc5d175b
Ian Nastajus <ian.nastajus@gmail.com>
2020-03-07 03:17:15 -0500

    naics selects basics

---
+++ queries_on_work.sql
@@ -0,0 +1,13 @@
+select * from naics.ca_structure WHERE ClassTitle LIKE '%software%';
+select * from naics.ca_structure WHERE ClassTitle LIKE '%information%';
+select * from naics.ca_structure WHERE ClassTitle LIKE '%develop%';
+-- select * from naics.ca_structure WHERE ClassTitle LIKE '%engineer%';
+-- select * from naics.ca_structure WHERE ClassTitle LIKE '%technical%';
+select * from naics.ca_structure WHERE ClassTitle LIKE '%data%';
+select * from naics.ca_structure WHERE ClassTitle LIKE '%science%';
+
+select * from naics.ca_structure WHERE ClassTitle LIKE '%game%'  AND Code LIKE '5%';
+
+select * from naics.ca_structure WHERE ClassTitle LIKE '%software%';
+select * from naics.ca_structure WHERE ClassTitle LIKE '%computer%';
+
