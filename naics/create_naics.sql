47e0de2a5d8d9840547dc8480a13098592893d25
Ian Nastajus <ian.nastajus@gmail.com>
2020-03-06 23:25:38 -0500

    NAICS stats canada's structure table added

--- naics/create_naics.sql
+++ naics/create_naics.sql
@@ -25,23 +25,34 @@ CREATE TABLE ca_element
 
 LOAD DATA INFILE 'D:/[[TO QUERY]]/NAICS/canada/naics-scian-2017-element-v3-eng.csv' IGNORE
     INTO TABLE ca_element
-    -- CHARACTER SET utf8mb4 -- insufficient to solve issue importing some row's special characters
     CHARACTER SET latin1 -- solves issue importing
     FIELDS TERMINATED BY ','
     OPTIONALLY ENCLOSED BY '"'
     LINES TERMINATED BY '\r\n'
     IGNORE 1 LINES
     (@skipLevel, code, classTitle, typeLabel, description);
--- [HY000][1300] Invalid utf8 character string: '"pig farming (i.e., swine, farrows, piglets, hogs,'
 
 
 DROP TABLE IF EXISTS ca_structure;
 CREATE TABLE ca_structure
 (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    -- Level,	HierarchicalStructure,	Code,	ClassTitle,	Superscript,	ClassDefinition
-
+    id                    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Level                 VARCHAR(1),
+    HierarchicalStructure VARCHAR(17),
+    Code                  VARCHAR(6),
+    ClassTitle            VARCHAR(128),
+    Superscript           VARCHAR(3),
+    ClassDefinition       VARCHAR(8192)
 );
+
+LOAD DATA INFILE 'D:/[[TO QUERY]]/NAICS/canada/naics-scian-2017-structure-v3-eng.csv' IGNORE
+    INTO TABLE ca_structure
+    CHARACTER SET latin1 -- solves issue importing
+    FIELDS TERMINATED BY ','
+    OPTIONALLY ENCLOSED BY '"'
+    LINES TERMINATED BY '\r\n'
+    IGNORE 1 LINES
+(Level, HierarchicalStructure, Code, ClassTitle, Superscript, ClassDefinition);
 
 DROP TABLE IF EXISTS ca_element;
 CREATE TABLE ca_element
